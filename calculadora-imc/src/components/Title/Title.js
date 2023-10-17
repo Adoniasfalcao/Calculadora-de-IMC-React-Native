c46f45f6e58699291a46148798d475777372442b
DhenzelNexxus <dhenzellimaribeiro@gmail.com>
2023-10-17 15:12:15 -0300

    Easter Egg

--- calculadora-imc/src/components/Title/Title.js
+++ calculadora-imc/src/components/Title/Title.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
 import styles from "./style";
 
 export default function Title() {
+  const [contador, setContador] = useState(0)
+
+  function Iterar(){
+    setContador(+1)
+  }
 
   return (
     <View style={styles.boxTitle}>
-      <Text style={styles.textTitle}>CÁLCULO DE IMC</Text>
+      <Text style={styles.textTitle} onPress={() => setContador(contador + 1)}>CÁLCULO DE IMC</Text>
+      {contador === 4 && 
+      <Text>MODO DESENVOLVEDOR ATIVADO</Text>}
     </View>
   );
   
